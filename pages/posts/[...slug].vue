5fbfc83c0ef406e2adb2af85ed73f0d98b102c98
Marcel Scognamiglio <67491786+marcelxv@users.noreply.github.com>
2023-11-09 21:19:48 -0300

    fix: lint adjusts

--- pages/posts/[...slug].vue
+++ pages/posts/[...slug].vue
@@ -1,10 +1,9 @@
 <template>
   <main class="container safe-bottom ">
-    <h1>{{ title }}</h1>
     <ContentDoc v-slot="{ doc }">
       <article>
         <h1>{{ doc.title }}</h1>
-        <img :src="doc.image" :alt="doc.title" class="img-fluid" />
+        <img :src="doc.image" :alt="doc.title" class="img-fluid">
         <ContentRenderer :value="doc" />
       </article>
     </ContentDoc>
@@ -13,16 +12,3 @@
     </RedirectButton>
   </main>
 </template>
-
-<script lang="ts" setup>
-import { ref } from 'vue'
-
-const slug = ref<string[]>([])
-
-const content = ref<string>('')
-
-const toc = ref<TocItem[]>([])
-
-const title = ref<string>('')
-
-</script>
